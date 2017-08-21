ac114416248e628b8b8361247518153d86a5ab29
july914 <july914@yandex.ru>
2017-08-21 17:57:00 +0300

    Added interface and controller for VK api

--- app/src/main/java/com/example/yuliya/demotestapp/ui/activities/MyAdapter.java
+++ app/src/main/java/com/example/yuliya/demotestapp/ui/activities/MyAdapter.java
@@ -8,9 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.yuliya.demotestapp.R;
+import com.example.yuliya.demotestapp.Models.Cat;
 import com.squareup.picasso.Picasso;
 
 
